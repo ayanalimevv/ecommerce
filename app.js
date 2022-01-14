a974f2d3e4da31dc9c4f39146b09944c19835313
Ayan ALi <22alpha07@gmail.com>
2022-01-14 23:39:43 +0530

    some mnor changes

--- app.js
+++ app.js
@@ -1,21 +1,56 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const bodyParser=require("body-parser");
-const ejs=require('ejs');
-const app=express();
-const { append } = require('vary');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const { append } = require("vary");
+const req = require("express/lib/request");
 
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-    res.render("index");
+const url = "mongodb://localhost:27017/alpha_store";
+mongoose.connect(url);
+let schema = mongoose.Schema({
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+let Store = mongoose.model("alpha_store", schema);
+
+app.get("/add",(req,res)=>{
+    res.render("add");
 })
-app.post("/",(req,res)=>{
-    console.log(req.body);
-    res.render("index");
-})
-app.listen('3000',()=>{
-    console.log("Listening");
-})
+app.post("/add",(req,res)=>{
+    let img=req.body.img;
+    let title=req.body.title;
+    let price=req.body.price;
+    let item= new Store({
+        img:img,
+        title:title,
+        price:price
+    });
+    item.save();
+    res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.render("index");
+});
+app.listen("3000", () => {
+  console.log("Listening");
+});
